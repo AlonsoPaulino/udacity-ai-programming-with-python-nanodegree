d4f4e065191b4a0831a2107786c8136811dd76bb
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-12-06 01:15:52 -0500

    adding importing

---
+++ Introduction to Python/importing/demo.py
@@ -0,0 +1,8 @@
+import other_script
+import useful_functions as uf
+
+print(4)
+print(other_script.num)
+
+scores = [20, 30, 40, 50]
+mean = uf.mean(scores)
---
+++ Introduction to Python/importing/useful_functions.py
@@ -0,0 +1,7 @@
+def mean(num_list):
+    return sum(num_list) / len(num_list)
+
+# Making sure that these lines will be executed only if this 
+# file is executed directly
+if __name__ == '__main__':
+    print("Useful cuntions!")
