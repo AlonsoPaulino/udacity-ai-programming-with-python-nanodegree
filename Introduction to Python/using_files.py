12f80fe397db504e0cf082f89226a7443b09d8a9
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-12-06 00:55:29 -0500

    add iterating through a file opened by with

--- Introduction to Python/using_files.py
+++ Introduction to Python/using_files.py
@@ -14,3 +14,19 @@ with open('another_file.txt', 'r') as f:
     file_data = f.read()
 
 print(file_data)
+
+#Each time we called read on the file with an integer argument, it read up to that number 
+#of characters, outputted them, and kept the 'window' at that position for the next call to read
+with open("another_file.txt") as song:
+    print(song.read(2))
+    print(song.read(8))
+    print(song.read())
+
+
+#Iterating through each file line. Deleting the end line using strip().
+camelot_lines = []
+with open("camelot.txt") as f:
+    for line in f:
+        camelot_lines.append(line.strip())
+
+print(camelot_lines)
