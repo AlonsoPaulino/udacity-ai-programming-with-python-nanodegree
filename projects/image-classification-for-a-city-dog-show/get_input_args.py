3c6e61602db46126af566df795fb1b1b478b1346
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-12-10 00:23:39 -0500

    add parser

--- projects/image-classification-for-a-city-dog-show/get_input_args.py
+++ projects/image-classification-for-a-city-dog-show/get_input_args.py
@@ -40,4 +40,14 @@ def get_input_args():
     """
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+
+    parser =  argparse.ArgumentParser()
+
+    parser.add_argument('--dir', type=str, default='pet_images/', help='path to the folder of pet images')
+    parser.add_argument('--arch', type=str, default='vgg', help='CNN model architecture to use')
+    parser.add_argument('--dogfile', type=str, default='dognames.txt', help='the file that contains the list of valid dognames')
+
+    in_args = parser.parse_args()
+    print(str(in_args))
+
+    return in_args
